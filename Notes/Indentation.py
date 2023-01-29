a0df533ace7037961b278a46a8fb32f64c9336dd
TehLily <tehlily@gmail.com>
2023-01-28 20:46:22 -0600

    Adding Topic 1

---
+++ Notes/Indentation.py
@@ -0,0 +1,10 @@
+#Sequential
+age = 16
+rating = 'g'
+#conditional
+if (rating == 'r' and age < 17):
+    print("You are too young to see the movie.")
+else:
+    print("Your age is fine to see the movie.")
+#sequential
+print("Done with program.")
