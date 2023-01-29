a63837f11df4f9989bf6b29e8e4b0aefa4c4772d
TehLily <tehlily@gmail.com>
2023-01-28 21:11:15 -0600

    Adding Assignments directory and If-ElifEllison file

---
+++ Assignments/If-ElifEllison.py
@@ -0,0 +1,30 @@
+"""
+Program: If-ElifEllison.py
+Author: Lily Ellison
+Last date modified: 01/28/2023
+
+The purpose of this program is to ask for the user to sign up for Programmer's Toolkit Monthly Subscription Box and have
+them select their level of membership and print out their selection and the cost.
+
+"""
+
+
+"""
+Each month is something different, t-shirts, stickers, figurines, even programming books!
+The levels are the following:
+
+    Platinum
+    Gold
+    Silver
+    Bronze
+    Free Trial
+
+Platinum is $60, each level below is 10 dollars cheaper, and the free trial is free. Write a program that uses a
+selection statement, getting user's input for a level and printing the cost for that level.
+Submit your .py file.
+As an example, after it describes the Subscription box levels, it then says "which level would you like to try?" if I
+enter "silver" or something equating to level silver - it should say something like "Silver is $40 a month. Thanks for
+selecting that"
+"""
+
+
